558f1e3a28599bd4001360b5761d372712a4f30a
Honey1b <nh822@naver.com>
2019-09-24 20:20:48 +0900

    n_queen,remotecontrol

--- 20190917/dice_3.cpp
+++ 20190917/dice_3.cpp
@@ -7,18 +7,18 @@ int check[7];
 void f(int level){
 	if(level > n){
 		for(int i=1;i<level;i++){
-			if(check[i]==1) return;
-			else 
-				printf("%d ",arr[level]);
-			}
+ 			printf("%d ",arr[level]);
+		}
 		printf("\n");
 		return;
 	}
 	for(int i=1;i<=6;i++){
-		arr[level]=i;
-		check[i]=1;
-		f(level+1);
-		check[i]=0;
+		if(check[i]==0){
+			arr[level]=i;
+			check[i]=1;
+			f(level+1);
+			check[i]=0;
+		}
 	}
 }
 
