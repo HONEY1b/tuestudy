9dc5abcfcf753f5a409d4c379f5beb0ca35f3bb1
honey1b <nh822@naver.com>
2020-01-07 19:00:55 +0900

    Update 2629.cpp

--- 20200103/2629.cpp
+++ 20200103/2629.cpp
@@ -1,14 +1,43 @@
 //2629
 #include<bits/stdc++.h>
-
+#define N 30
 using namespace std;
 
+int n,w[N+1],t,tt[8];
+int check[N+1][N*500+1];
 
+int ABS(int x) {return (x>0)? x:-x;}
+
+void f(int cnt,int weight){
+	if(cnt>n) return;
+	if(check[cnt][weight]) return;
+
+	check[cnt][weight]=1;
+	
+	f(cnt+1,weight+w[cnt+1]);
+	f(cnt+1,weight);
+	f(cnt+1,ABS(weight-w[cnt+1]));	
+}
 
 int main(void){
 	
-	freopen("2629.txt","r",stdin);
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
 	
+	freopen("2629.txt","r",stdin);
+	cin>>n;
+	for(int i=1;i<=n;i++) cin>>w[i];
+	
+	cin>>t;
+	for(int i=1;i<=t;i++) cin>>tt[i];
+	
+	f(0,0);
+	
+	for(int i=1;i<=t;i++){
+		if(tt[i] > 40000) cout<<"N\n";
+		else if(check[n][tt[i]]) cout<<"Y\n";
+		else cout<<"N\n";	
+	}
 	
 	return 0;
 }
