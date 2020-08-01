c61a68c93f40a7781771e7ee47d2a7595960f815
honey1b <nh822@naver.com>
2020-08-01 16:18:01 +0900

    20200801

--- acmicpc/10816.cpp
+++ acmicpc/10816.cpp
@@ -1,14 +1,38 @@
 //10816
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> v(20000010);
+vector<pair<int,int> > v2;
+int Max=987654321;
+int n,m;
 
-int arr[500000];
-int n,t;
-
+bool cmp(pair<int,int> p1,pair<int, int> p2){
+	return p1.second < p2.second;
+}
 
 int main(void){
-	freopen("10815.txt","r",stdin);
-	scanf("%d",&n);	
+	freopen("10816.txt","r",stdin);
+	scanf("%d",&n);
 	for(int i=0;i<n;i++){
-		scanf("%d",arr[i]);	
+		int a;
+		scanf("%d",&a);
+		v[a+10000000]++;
+	}
+	
+	scanf("%d",&m);
+	for(int i=0;i<m;i++){
+		int a;
+		scanf("%d",&a);
+		v2.push_back(make_pair(i,a));
+	}
+	
+	//입력 받은 거 내림차순으로 정렬 
+	//sort(v2.begin(),v2.end(),cmp);
+
+	//for(int i=0;i<v2.size();i++){printf("%d\t",v2[i].second);}
+	//printf("\n");
+	for(int i=0;i<v2.size();i++){
+		printf("%d ",v[v2[i].second + 10000000]);	
 	}
 	
 	return 0;
